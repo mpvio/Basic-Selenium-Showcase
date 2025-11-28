85861dfc12ba63da42a00b0b3fbd679c4dc39c37
mpvio <suryaragavan2@outlook.com>
2025-11-28 11:32:24 +0000

    added tab switching example

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 ### Simple showcase of Selenium and BeautifulSoup usage in Python (using Firefox as the browser of choice).
 
-The code loads [example.com](https://www.example.com) and showcases:
+example_com loads [example.com](https://www.example.com) and showcases:
 * Scraping text from webpages
 * Clicking links to new pages
 * Waiting for those pages to load
 * Entering text
 * Using Beautiful Soup to parse more complex page elements
+
+tab_switching loads the above and [my GitHub](https://github.com/mpvio) and showcases:
+* Running Javascript code
+* Using window handles to track tabs
+* Tab switching
+* Scraping information from multiple tabs
+* Closing specific tabs while keeping the driver running
--- main.py
+++ main.py
@@ -55,5 +55,34 @@ def example_com():
             if title is not None:
                 print(title.get_text())
 
+def tab_switching():
+    options = webdriver.FirefoxOptions()
+    options.add_argument("--headless")
+
+    with webdriver.Firefox(options=options) as driver:
+        driver.get("https://www.example.com")
+        main_handle = driver.current_window_handle
+
+        # open second tab
+        driver.execute_script("window.open('https://github.com/mpvio', 'second')")
+        WebDriverWait(driver, 10).until(EC.number_of_windows_to_be(2))
+        driver.switch_to.window("second")
+        second_handle = driver.current_window_handle
+        print(f"Current window handle: {second_handle}")
+
+        # get info from second tab
+        repository_count = driver.find_element(By.XPATH, "/html/body/div[1]/div[4]/main/div[1]/div/div/div[2]/div/nav/a[2]/span").text
+        repo_word = "repository" if repository_count == 1 else "repositories"
+        print(f"I have {repository_count} {repo_word}.")
+
+        # switch back to first tab
+        driver.close()
+        driver.switch_to.window(main_handle)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "body > div:nth-child(1) > h1:nth-child(1)")))
+        element = driver.find_element(By.CSS_SELECTOR, "body > div:nth-child(1) > h1:nth-child(1)").text
+        print(f"Current handle: {main_handle}. Current page: {element}.")
+
+
 if __name__ == "__main__":
     example_com()
+    tab_switching()
